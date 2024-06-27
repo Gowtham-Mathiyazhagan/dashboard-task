a0f4d28bc54dfb76964e6d3711fed6d626436c90
Gowtham-Mathiyazhagan <gowthammathi07@gmail.com>
2024-06-27 18:31:00 +0530

    feat : aside section implemented successfully

---
+++ src/Components/Aside.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import WebTitle from "../assets/images/WebTitle";
+import "../assets/css/aside.css";
+
+const Aside = () => {
+  return (
+    <aside className="aside-container">
+      <header className="header">
+        <WebTitle />
+      </header>
+      <ul className="products">
+        <li className="title">
+          <h4>home</h4>
+        </li>
+        <li className="product">
+          <a href="#">dashboard</a>
+        </li>
+      </ul>
+
+      <ul className="products">
+        <li className="title">
+          <h4>resource</h4>
+        </li>
+        <li className="product">
+          <a href="#">add resource</a>
+          <a href="#">view resource</a>
+        </li>
+      </ul>
+
+      <ul className="products">
+        <li className="title">
+          <h4>report</h4>
+        </li>
+        <li className="product">
+          <a href="#">JL</a>
+          <a href="#">track</a>
+          <a href="#">app reports</a>
+          <a href="#">bundle wish</a>
+          <a href="#">full report</a>
+          <a href="#">allocation</a>
+          <a href="#">Forecast Vs Actual Allocation</a>
+          <a href="#">buffer resource</a>
+        </li>
+      </ul>
+    </aside>
+  );
+};
+
+export default Aside;
