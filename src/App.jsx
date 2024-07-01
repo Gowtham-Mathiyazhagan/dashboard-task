50a94d24a98a87c34b504b74428defdf8957f79f
Gowtham-Mathiyazhagan <gowthammathi07@gmail.com>
2024-07-01 19:25:09 +0530

    fix : changes are added to repo

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,35 @@ import React, { useState } from "react";
 import Aside from "./Components/Aside";
 import Navbar from "./Components/Navbar.jsx";
 import Content from "./Components/Content.jsx";
+import AddResource from "./Components/AddResource.jsx";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-  const [active, setActive] = useState(false)
+  const [active, setActive] = useState(false);
   return (
-    <main onClick={()=>setActive(false)}>
-      <Aside />
-      <Navbar inputChanges={{active,setActive}}/>
-      <Content/>
+    <main onClick={() => setActive(false)}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Aside />
+              <Navbar inputChanges={{ active, setActive }} />
+              <Content />
+            </>
+          }
+        />
+        <Route
+          path="/addresource"
+          element={
+            <>
+              <Aside />
+              <Navbar inputChanges={{ active, setActive }} />
+              <AddResource />
+            </>
+          }
+        />
+      </Routes>
     </main>
   );
 };
--- src/Components/Aside.jsx
+++ src/Components/Aside.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import "../assets/css/aside.css";
+import { Link } from "react-router-dom";
 
 const Aside = () => {
-  const [active, setActive] = useState(1)
+  const [active, setActive] = useState(1);
   const handleClick = (e) => {
     if (e.target.tagName === "A") {
-   setActive(Number(e.target.className))
+      setActive(Number(e.target.className));
     }
   };
 
@@ -16,7 +17,7 @@ const Aside = () => {
           <h4>home</h4>
         </li>
         <li className="product">
-          <a href="#" className={active === 1 ? "1 active" : "1" }>dashboard</a>
+        <Link className="link" to={'/'}><a className={active === 1 ? "1 active" : "1"}>dashboard</a></Link >
         </li>
       </ul>
 
@@ -25,8 +26,8 @@ const Aside = () => {
           <h4>resource</h4>
         </li>
         <li className="product">
-          <a href="#" className={active === 2 ? "2 active" : "2" }>add resource</a>
-          <a href="#" className={active === 3 ? "3 active" : "3" }>view resource</a>
+          <Link className="link" to={'/addresource'}><a className={active === 2 ? "2 active" : "2"}>add resource</a></Link>
+          <a className={active === 3 ? "3 active" : "3"}>view resource</a>
         </li>
       </ul>
 
@@ -35,14 +36,16 @@ const Aside = () => {
           <h4>report</h4>
         </li>
         <li className="product">
-          <a href="#" className={active === 4 ? "4 active" : "4" }>JL</a>
-          <a href="#" className={active === 5 ? "5 active" : "5" }>track</a>
-          <a href="#" className={active === 6 ? "6 active" : "6" }>app reports</a>
-          <a href="#" className={active === 7 ? "7 active" : "7" }>bundle wish</a>
-          <a href="#" className={active === 8 ? "8 active" : "8" }>full report</a>
-          <a href="#" className={active === 9 ? "9 active" : "9" }>allocation</a>
-          <a href="#" className={active === 10 ? "10 active" : "10" }>Forecast Vs Actual Allocation</a>
-          <a href="#" className={active === 11 ? "11 active" : "11" }>buffer resource</a>
+          <a className={active === 4 ? "4 active" : "4"}>JL</a>
+          <a className={active === 5 ? "5 active" : "5"}>track</a>
+          <a className={active === 6 ? "6 active" : "6"}>app reports</a>
+          <a className={active === 7 ? "7 active" : "7"}>bundle wish</a>
+          <a className={active === 8 ? "8 active" : "8"}>full report</a>
+          <a className={active === 9 ? "9 active" : "9"}>allocation</a>
+          <a className={active === 10 ? "10 active" : "10"}>
+            Forecast Vs Actual Allocation
+          </a>
+          <a className={active === 11 ? "11 active" : "11"}>buffer resource</a>
         </li>
       </ul>
     </aside>
