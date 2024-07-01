900c3e018ea1ac9d6fb11e61868afbf28403ec86
Gowtham-Mathiyazhagan <gowthammathi07@gmail.com>
2024-07-01 19:24:09 +0530

    feat : route integrated

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.min.css"
 import App from './App.jsx'
 import './App.css'
+import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
