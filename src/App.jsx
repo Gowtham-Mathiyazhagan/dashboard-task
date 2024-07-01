854f0225e0f9ee22a5c32171187b6711d8228741
Gowtham-Mathiyazhagan <gowthammathi07@gmail.com>
2024-07-01 15:21:38 +0530

    fix : changes added to repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Aside from "./Components/Aside";
-
+import Navbar from "./Components/Navbar.jsx";
+import Content from "./Components/Content.jsx";
 
 const App = () => {
+  const [active, setActive] = useState(false)
   return (
-    <main>
+    <main onClick={()=>setActive(false)}>
       <Aside />
-      
+      <Navbar inputChanges={{active,setActive}}/>
+      <Content/>
     </main>
   );
 };
